0e8166ad5b275f03cbbcf24a6549805f19c69887
dipendra <dewangandipendradewangan@gmail.com>
2022-10-07 08:50:11 +0530

    created mail system using to localstorage email data and tested email using dotenv file

---
+++ ch-25/routing/node-api/verifyToken.js
@@ -0,0 +1,61 @@
+const query = require("querystring");
+const jwt = require("jsonwebtoken");
+const database = require("./db");
+
+exports.result = (request, response) => {
+    let formdata = "";
+
+    request.on("data", (chunks) => {
+        formdata += chunks.toString();
+    });
+
+    request.on("end", () => {
+        const post = query.parse(formdata);
+
+        if (post.token && post.token != "") {
+
+            const secret_Id = post.secretId;
+            const findReq = database.findById(secret_Id, "jwt_secrets");
+            findReq.then((successRes) => {
+
+                const secret = successRes.data[0].secret;
+
+                // verify the token
+                const teken = jwt.verify(post.token, secret, (error, success) => {
+                    if (success) {
+                        const message = JSON.stringify({
+                            isVerified: true,
+                            message: "Token verified!",
+                        });
+                        sendResponse(response, message, 200);
+                        
+                    } else {
+                        const message = JSON.stringify({
+                            isVerified: false,
+                            message: "Token not verified!",
+                        });
+                        sendResponse(response, message, 401);
+                    }
+                });
+            }).catch((errorRes) => {
+                console.log(errorRes);
+            });
+
+        } else {
+            const message = JSON.stringify({
+                isVerified: false,
+                message: "Unauthorised user!",
+            });
+            sendResponse(response, message, 401);
+        }
+    })
+
+    const sendResponse = (response, message, statusCode) => {
+        response.writeHead(statusCode, {
+            'Content-type': 'application/json'
+        });
+
+        response.write(message);
+        return response.end();
+    }
+}
